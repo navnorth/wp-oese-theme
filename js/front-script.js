d05f20b7741a677b2456681a6d50290a78782594
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2015-05-20 14:51:09 +0800

    updated RegEx on checking external links and mailto. fixes #8

--- js/front-script.js
+++ js/front-script.js
@@ -56,7 +56,7 @@ jQuery( document ).ready(function() {
 
     // set external links to open in new window and have distinct style
     jQuery('a').each(function() {
-        var a = new RegExp('/' + window.location.host + '|mailto/i');
+	var a = new RegExp('' + window.location.host + '|mailto' , 'i');
         if(!a.test(this.href)) {
             jQuery(this).attr( 'target','_blank' );
             jQuery(this).addClass( 'external_link' );
