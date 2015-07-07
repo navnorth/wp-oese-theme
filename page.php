82ab09b2ae8440a9a553eaf9f012b787f1ee144e
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2015-07-07 23:17:15 +0800

    Updated default template to display featured image

--- page.php
+++ page.php
@@ -14,12 +14,21 @@
 
 get_header();
 global $post;
+$page_id = get_the_ID();
+$img_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+$img_alt = get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true);
 ?>
 
 	<div id="content" class="row site-content">
 	
         <div class="col-md-9 c ol-sm-12 col-xs-12 padding_left lft_sid_cntnr">
 		<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb('<p id="breadcrumbs">','</p>'); }  ?>
+		<?php
+                if(isset($img_url) && !empty($img_url))
+                {
+                    echo '<div class="program_header_image"><img src="'. $img_url .'" alt="'.$img_alt. '" /></div>';
+		}
+		?>
         	<h1 class="page_header"><?php echo $post->post_title;?></h1>
             <div class="share_links_header"><?php echo do_shortcode("[ssba]"); ?></div>
 
