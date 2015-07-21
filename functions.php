4c85be096c6d3d21043fc66871bc7966193ad33e
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2015-07-21 22:50:01 +0800

    create news categories widget in lieu of the core Categories Widget

--- functions.php
+++ functions.php
@@ -226,6 +226,15 @@ function register_oii_widget(){
     register_widget( 'Related_Posts_Widget' );
 }
 
+/**
+ * News Categories Widget
+ **/
+require_once( get_stylesheet_directory() . '/widgets/news_categories_widget.php' );
+add_action( 'widgets_init' , 'register_news_categories_widget' );
+function register_news_categories_widget(){
+    register_widget( 'News_Categories_Widget' );
+}
+
 /* minor style changes to News "Continue Reading" links */
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
 function modify_read_more_link() {
@@ -286,6 +295,118 @@ function taxonomies_for_pages() {
 
 function related_posts_where( $where ) {
     return $where." AND post_type='post'";
+}
+
+// altered wp_list_categories
+function wp_list_categories_for_posts( $args = '' ) {
+    $defaults = array(
+	    'show_option_all' => '', 'show_option_none' => __('No categories'),
+	    'orderby' => 'name', 'order' => 'ASC',
+	    'style' => 'list',
+	    'show_count' => 0, 'hide_empty' => 1,
+	    'use_desc_for_title' => 1, 'child_of' => 0,
+	    'feed' => '', 'feed_type' => '',
+	    'feed_image' => '', 'exclude' => '',
+	    'exclude_tree' => '', 'current_category' => 0,
+	    'hierarchical' => true, 'title_li' => __( 'Categories' ),
+	    'echo' => 1, 'depth' => 0,
+	    'taxonomy' => 'category'
+    );
+
+    $r = wp_parse_args( $args, $defaults );
+
+    if ( !isset( $r['pad_counts'] ) && $r['show_count'] && $r['hierarchical'] )
+	$r['pad_counts'] = true;
+
+    if ( true == $r['hierarchical'] ) {
+	$r['exclude_tree'] = $r['exclude'];
+	$r['exclude'] = '';
+    }
+
+    if ( ! isset( $r['class'] ) )
+	$r['class'] = ( 'category' == $r['taxonomy'] ) ? 'categories' : $r['taxonomy'];
+
+    if ( ! taxonomy_exists( $r['taxonomy'] ) ) {
+	return false;
+    }
+
+    $show_option_all = $r['show_option_all'];
+    $show_option_none = $r['show_option_none'];
+
+    $categories = get_categories( $r );
+    foreach ($categories as $key => $category){
+       add_filter( 'posts_where' , 'related_posts_where' );
+	$rPosts = new WP_Query(array(
+		'post_type'             => array('post'),
+		'is_single'             => true,
+		'no_found_rows'         => true,
+		'post_status'           => array('publish'),
+		'ignore_sticky_posts'   => true,
+		'cat'                   => $category->cat_ID
+	));
+    
+	// If no posts found, ...
+	if ($rPosts->post_count==0) {
+	    unset($categories[$key]);
+	}
+    }
+
+    $output = '';
+    if ( $r['title_li'] && 'list' == $r['style'] ) {
+	    $output = '<li class="' . esc_attr( $r['class'] ) . '">' . $r['title_li'] . '<ul>';
+    }
+    if ( empty( $categories ) ) {
+	    if ( ! empty( $show_option_none ) ) {
+		    if ( 'list' == $r['style'] ) {
+			    $output .= '<li class="cat-item-none">' . $show_option_none . '</li>';
+		    } else {
+			    $output .= $show_option_none;
+		    }
+	    }
+    } else {
+	    if ( ! empty( $show_option_all ) ) {
+		    $posts_page = ( 'page' == get_option( 'show_on_front' ) && get_option( 'page_for_posts' ) ) ? get_permalink( get_option( 'page_for_posts' ) ) : home_url( '/' );
+		    $posts_page = esc_url( $posts_page );
+		    if ( 'list' == $r['style'] ) {
+			    $output .= "<li class='cat-item-all'><a href='$posts_page'>$show_option_all</a></li>";
+		    } else {
+			    $output .= "<a href='$posts_page'>$show_option_all</a>";
+		    }
+	    }
+
+	    if ( empty( $r['current_category'] ) && ( is_category() || is_tax() || is_tag() ) ) {
+		    $current_term_object = get_queried_object();
+		    if ( $current_term_object && $r['taxonomy'] === $current_term_object->taxonomy ) {
+			    $r['current_category'] = get_queried_object_id();
+		    }
+	    }
+
+	    if ( $r['hierarchical'] ) {
+		    $depth = $r['depth'];
+	    } else {
+		    $depth = -1; // Flat.
+	    }
+	    $output .= walk_category_tree( $categories, $depth, $r );
+    }
+
+    if ( $r['title_li'] && 'list' == $r['style'] )
+	    $output .= '</ul></li>';
+
+    /**
+     * Filter the HTML output of a taxonomy list.
+     *
+     * @since 2.1.0
+     *
+     * @param string $output HTML output.
+     * @param array  $args   An array of taxonomy-listing arguments.
+     */
+    $html = apply_filters( 'wp_list_categories', $output, $args );
+    wp_reset_postdata();
+    if ( $r['echo'] ) {
+	    echo $html;
+    } else {
+	    return $html;
+    }
 }
 
 if (is_admin()) {
