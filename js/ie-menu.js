463278cb96a389c8f9e253ccb89ae2b95b233994
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2016-09-21 22:34:54 +0800

    add offset for mega menu spacing

--- js/ie-menu.js
+++ js/ie-menu.js
@@ -3,9 +3,10 @@ jQuery( document ).ready(function() {
     var top_submenus = itemmenu.children().length;
     var total_submenus = itemmenu.find("li").length;
     var i = 0, n = 0, r = 0;
-    var menu_height = 35;
+    var menu_height = 32;
     var new_top=0;
     var add_menuclass = false;
+    var offset = 0;
     itemmenu.find("li").each(function(index){
         var $this = jQuery(this);
         var $siblings;
@@ -23,16 +24,18 @@ jQuery( document ).ready(function() {
                 add_menuclass=false;
             } else {
                 if ($this.hasClass("menu-item-has-children")){
-                    new_top = r*menu_height - 10;
+                    offset += 10;
+                    new_top = r*menu_height - offset;
                     $this.addClass("menu-right").attr("style","position:absolute !important;border-right:none;top:" + new_top + "px");
                     r++;
                 } else {
-                    new_top = r*menu_height;
+                    new_top = r*menu_height - offset;
                     if ($this.parents().length==10) {
                         $this.addClass("menu-right").attr("style","position:absolute !important;border-right:none;top:" + new_top + "px;");
                     } else {
                         $this.attr("style","width:461px !important;float:none !important; border-right:none; display:block !important;");
                         $this.find("a").attr("style","margin-top:-5px;");
+                        offset += 5;
                     }
                     r++;
                 }
