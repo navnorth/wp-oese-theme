2b7e599af856f038c109055a7c52e00ec71536b3
joe hobson <joe@navigationnorth.com>
2016-09-21 14:42:23 -0500

    tweak IE again

--- js/ie-menu.js
+++ js/ie-menu.js
@@ -3,10 +3,10 @@ jQuery( document ).ready(function() {
     var top_submenus = itemmenu.children().length;
     var total_submenus = itemmenu.find("li").length;
     var i = 0, n = 0, r = 0;
-    var menu_height = 32;
+    var menu_height = 33;
     var new_top=0;
     var add_menuclass = false;
-    var offset = 0;
+    var offset = -8;
     itemmenu.find("li").each(function(index){
         var $this = jQuery(this);
         var $siblings;
@@ -31,11 +31,12 @@ jQuery( document ).ready(function() {
                 } else {
                     new_top = r*menu_height - offset;
                     if ($this.parents().length==10) {
+                        new_top -= 14;
                         $this.addClass("menu-right").attr("style","position:absolute !important;border-right:none;top:" + new_top + "px;");
                     } else {
                         $this.attr("style","width:461px !important;float:none !important; border-right:none; display:block !important;");
                         $this.find("a").attr("style","margin-top:-5px;");
-                        offset += 5;
+                        offset += 3;
                     }
                     r++;
                 }
