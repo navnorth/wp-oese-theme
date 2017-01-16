dc6499f5b26940c7de1b58dd8bf64405c0559682
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-01-16 23:48:39 +0800

    add custom nav menu walker class

--- header.php
+++ header.php
@@ -112,7 +112,7 @@
 
         <div class="row navi_bg">
         	<div class="main-menu">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'walker' => new oii_walker_nav_menu ) ); ?>
             </div>
 
             <span class="navi_icn fa-stack"><i class="fa fa-bars fa-stack-2x"></i></span>
