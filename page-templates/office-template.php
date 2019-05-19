38d2c6eec15d6da3cd32129693bea587f7553f2e
Jess Hathaway <hathaway.jess@gmail.com>
2019-05-19 11:18:55 -0700

    display featured image on office template

--- page-templates/office-template.php
+++ page-templates/office-template.php
@@ -13,6 +13,14 @@ $page_id = get_the_ID();
             <div class="col-md-8">
                 <div class="left-description-section">
                     <h1 class="h1-bottom-space"><?php echo get_the_title(); ?></h1>
+                    <?php
+                        if ( has_post_thumbnail() ) {
+                            $image = wp_get_attachment_image_src( get_post_thumbnail_id($page_id), 'single-post-thumbnail' );
+
+                            echo "<div class='left-section-featured-image'>
+                                    <img src=".$image[0]."></div>";
+                        }
+                    ?>
                      <?php
                      while (have_posts()) : the_post(); get_template_part('content', 'page'); 
                      endwhile;
