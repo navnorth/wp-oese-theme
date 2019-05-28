d64317d4de2b10155ecb471503cc2dc05c2ab2d2
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-05-28 21:43:57 +0800

    create publication template

---
+++ page-templates/publication-template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Publication Template
+ */
+
+global $post;
+
+$page_id = get_the_ID();
+get_header();
+?>
+    <!--Publication Template Top Section START-->
+    <div id="content" class="row custom-common-padding office-template">
+    </div>
+<?php get_footer(); ?>
