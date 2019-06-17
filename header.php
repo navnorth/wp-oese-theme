e5243df1809356030b63f047e0354bfadd395cb1
joe hobson <joe@navigationnorth.com>
2019-06-17 16:38:44 -0500

    revert header logo alt for SEO

--- header.php
+++ header.php
@@ -43,12 +43,12 @@
                 <div class="seperate-dark-blue-border"></div>
             </div>
         </div>
-    
+
         <div class="row top-nav-spacing">
            <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 col-9">
                 <a class="oese-logo-home" href="<?php echo site_url(); ?>" Title="Home">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg"
-                         alt="<?php echo $site_title;  ?>" class="svg-replace" id="header_logo"/>
+                         alt="U.S. Department of Education United States of America, Office of Elementary &amp; Secondary Education" class="svg-replace" id="header_logo"/>
                 </a>
             </div>
             <div class="col-xl-3 col-lg-3 col-md-4 col-sm-12 col-12 offset-xl-2 offset-lg-2 offset-md-1 d-sm-inline-block d-block d-md-block d-lg-block d-xl-block">
