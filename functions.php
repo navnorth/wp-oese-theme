8f5c8ce18dff2f36ae9a0fa2403c8464852c984c
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-08-13 21:56:13 +0800

    remove other dump vars

--- functions.php
+++ functions.php
@@ -2086,13 +2086,12 @@ function get_count_by_template($template_name) {
 function oese_action_solarium_query( $parameters ) {
   echo "<div style='display:none;'>";
   var_dump($parameters);
+  
   /* @var WPSOLR_Query $wpsolr_query */
   $wpsolr_query = $parameters[ WPSOLR_Events::WPSOLR_ACTION_SOLARIUM_QUERY__PARAM_WPSOLR_QUERY ];
-  var_dump($wpsolr_query);
   
   /* @var WPSOLR_AbstractSearchClient $search_engine_client */
   $search_engine_client = $parameters[ WPSOLR_Events::WPSOLR_ACTION_SOLARIUM_QUERY__PARAM_SOLARIUM_CLIENT ];
-  var_dump($search_engine_client);
   echo "</div>";
   
   // post_type url parameter
