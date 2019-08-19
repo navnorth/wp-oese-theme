dbd662fd002db474c101610a1b3dd45cc710a098
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-08-19 20:21:41 +0800

    change facet value to to template name

--- functions.php
+++ functions.php
@@ -2050,7 +2050,7 @@ function oese_get_page_types(){
   $facets = null;
   $templates = wp_get_theme()->get_page_templates();
   foreach ( $templates as $template_name => $template_filename ) {
-    $facets[] = array( "value" => $template_filename, "count" => get_count_by_template($template_name), "items" => null, "selected" => false, "value_localized" => $template_filename );
+    $facets[] = array( "value" => $template_name, "count" => get_count_by_template($template_name), "items" => null, "selected" => false, "value_localized" => $template_name );
   }
   
   return array(
