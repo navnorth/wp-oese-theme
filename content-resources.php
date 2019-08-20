1c9f781835159bfa2746bf6fedbf1a3e39572240
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-08-20 22:25:38 +0800

    update program sidebars with single level subpages

--- content-resources.php
+++ content-resources.php
@@ -1,9 +1,10 @@
 <?php
 //Initial Checking for showing the resources box
 
-
+$parentLink = null;
 $withChild = false;
-$subpages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'menu_order', 'sort_order' => 'asc' ) );
+//$subpages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'menu_order', 'sort_order' => 'asc' ) );
+$subpages = get_pages( array( 'child_of' => $post->ID, 'depth' => 1, 'sort_column' => 'menu_order', 'sort_order' => 'asc' ) );
 if ($subpages)
 	$withChild = true;
 else {
@@ -17,8 +18,10 @@ if($post->post_parent == 0){
 }
 else{
 	$template = get_page_template_slug($post->post_parent);
-	if ($template=="page-templates/program-template.php")
+	if ($template=="page-templates/program-template.php"){
 		$sidebarTitle = get_the_title($post->post_parent);
+		$parentLink = get_the_permalink($post->post_parent);
+	}
 	else
 		$sidebarTitle = $post->post_title;
 }
@@ -27,7 +30,11 @@ else{
 <div class="secondary-navigation-menu" id="toc">
 	<?php if ($withChild): ?>
 		<div class="secondary-navigation-menu-header">
+			<?php if ($parentLink): ?>
+			<h2><a href="<?php echo $parentLink; ?>"><?php echo $sidebarTitle; ?></a></h2>
+			<?php else: ?>
 			<h2><?php echo $sidebarTitle; ?></h2>
+			<?php endif; ?>
 		</div>
 		<ul class="secondary-navigation-menu-list">
 				<?php
@@ -43,7 +50,7 @@ else{
 						if ($parent_id>0) {
 							$parent_page = get_page($parent_id);
 							//Display Sub page links
-							$subpages = get_pages( array( 'child_of' => $parent_id, 'sort_column' => 'menu_order', 'sort_order' => 'asc', 'parent' => $parent_id ) );
+							$subpages = get_pages( array( 'child_of' => $parent_id, 'depth' => 1, 'sort_column' => 'menu_order', 'sort_order' => 'asc', 'parent' => $parent_id ) );
 
 							foreach($subpages as $spage) {
 								if ($post->ID==$spage->ID){
