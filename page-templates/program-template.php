2eff7ed5cc7cc519a4ad18478ca9436ab151cf69
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-09-27 02:39:41 +0800

    return get queried object id

--- page-templates/program-template.php
+++ page-templates/program-template.php
@@ -25,7 +25,7 @@ if ($archived_date){
     $head_class = " archived-header";
 }
 
-$spages = get_pages( array( 'parent' => $page_id, 'sort_column' => 'menu_order', 'sort_order' => 'asc' ) );
+$spages = get_pages( array( 'parent' => get_queried_object_id(), 'sort_column' => 'menu_order', 'sort_order' => 'asc' ) );
 var_dump($spages);
 if ($spages || (!empty($contactAddress)) || (!empty($contactPhone)) || (!empty($contactFax)) || (have_rows('sidebar_links'))){
     $leftCol = "col-md-8";
