2162aef6bbe0c559397a28509f189d493243fc1e
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-09-28 03:18:48 +0800

    re-arrange home page

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -16,48 +16,28 @@
                      endwhile;
                 endif;
             ?>
-
-            <?php if( have_rows('categories') ):  ?>
-
-                <div class="col-md-12 home-grid-section">
-                    <ul class="row">
-                        <?php
-                            while ( have_rows('categories') ) : the_row();
-                                 $cImage =  get_sub_field('c_image');
-                                 $cTitle =  get_sub_field('c_title');
-                                 $cLink =  get_sub_field('link');
-                                 $externaLink =  get_sub_field('c_external_link');
-                                 $target = ($externaLink ? "_blank" : "_self");
-                                 if (empty($cImage))
-                                    $cImage = get_stylesheet_directory_uri()."/images/link-icon.png";
-                                 else {
-                                    $cImage = wp_get_attachment_image_url($cImage,"medium");
-                                 }
-                        ?>
-                        <li class="col-md-4 pl-0 pr-0 ml-0 mr-0 home-col-md-4">
-                            <div class="custom-home-image-section">
-                                <div class="custom-image-media">
-                                    <a target="<?php echo $target; ?>" href="<?php echo $cLink ?>">
-                                        <div class="custom-image-thumbnail">
-                                            <div>
-                                                <img src="<?php echo $cImage ;?>" alt="<?php echo $cTitle; ?>" class="img-responsive img-thumbnail-square">
-                                            </div>
-                                        </div>
-                                        <div class="custom-home-image-heading text-center">
-                                            <p><?php echo $cTitle; ?></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </li>
-                        <?php endwhile; ?>
-                    </ul>
-                    </div>
-                </div>
-            <?php endif; ?>
+            
+         </div>
         <!--</div>-->
 
-
+         <!--Full width Search Section START-->
+         <div class="row custom-common-padding">
+             <div class="full-search-section m-auto text-center">
+                 <div class="full-search-heading">
+                     <h1>What can we help you find?</h1>
+                 </div>
+ 
+                 <div class="full-search-field">
+                      <?php
+                      // Call Custom Search Form
+                      add_filter('get_search_form', 'oese_content_search_form');
+                      get_search_form();
+                      remove_filter('get_search_form', 'oese_content_search_form');
+                      ?>
+                 </div>
+             </div>
+         </div>
+         <!--Full width Search Section END-->
         <!-- Div seperator-->
         <!-- <div class="row mr-0 ml-0">
             <div class="col-md-12 pr-0 pl-0">
@@ -80,33 +60,7 @@
             </div>
         </div>
         <!--Div seperator END-->
-
-         <!--Full width Search Section START-->
-        <div class="row custom-common-padding">
-            <div class="full-search-section m-auto text-center">
-                <div class="full-search-heading">
-                    <h1>What can we help you find?</h1>
-                </div>
-
-                <div class="full-search-field">
-                     <?php
-                     // Call Custom Search Form
-                     add_filter('get_search_form', 'oese_content_search_form');
-                     get_search_form();
-                     remove_filter('get_search_form', 'oese_content_search_form');
-                     ?>
-                </div>
-            </div>
-        </div>
-        <!--Full width Search Section END-->
-
-        <!--Div seperator-->
-        <div class="row mr-0 ml-0">
-            <div class="col-md-12 pr-0 pl-0">
-                <div class="seperate-dark-blue-border"></div>
-            </div>
-        </div>
-        <!--Div seperator END-->
+        
         <?php if( have_rows('trending_now') ):  ?>
 
         <div class="row custom-common-padding trending-now mr-0 ml-0">
@@ -160,6 +114,52 @@
 
        <?php endif; ?>
         <!--Audience Page Custom Design - END-->
+
+        <!--Div seperator-->
+        <div class="row mr-0 ml-0">
+            <div class="col-md-12 pr-0 pl-0">
+                <div class="seperate-dark-blue-border"></div>
+            </div>
+        </div>
+        <!--Div seperator END-->
+        
+        <?php if( have_rows('categories') ):  ?>
+
+                <div class="col-md-12 home-grid-section">
+                    <ul class="row">
+                        <?php
+                            while ( have_rows('categories') ) : the_row();
+                                 $cImage =  get_sub_field('c_image');
+                                 $cTitle =  get_sub_field('c_title');
+                                 $cLink =  get_sub_field('link');
+                                 $externaLink =  get_sub_field('c_external_link');
+                                 $target = ($externaLink ? "_blank" : "_self");
+                                 if (empty($cImage))
+                                    $cImage = get_stylesheet_directory_uri()."/images/link-icon.png";
+                                 else {
+                                    $cImage = wp_get_attachment_image_url($cImage,"medium");
+                                 }
+                        ?>
+                        <li class="col-md-4 pl-0 pr-0 ml-0 mr-0 home-col-md-4">
+                            <div class="custom-home-image-section">
+                                <div class="custom-image-media">
+                                    <a target="<?php echo $target; ?>" href="<?php echo $cLink ?>">
+                                        <div class="custom-image-thumbnail">
+                                            <div>
+                                                <img src="<?php echo $cImage ;?>" alt="<?php echo $cTitle; ?>" class="img-responsive img-thumbnail-square">
+                                            </div>
+                                        </div>
+                                        <div class="custom-home-image-heading text-center">
+                                            <p><?php echo $cTitle; ?></p>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                        <?php endwhile; ?>
+                    </ul>
+                  </div>
+            <?php endif; ?>
 
 
 <?php get_footer();?>
