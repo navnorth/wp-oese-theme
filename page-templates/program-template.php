c1deca8fe6b229c69e875f04852a214b78dd896d
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-10-16 00:16:33 +0800

    another experiment for iPad

--- page-templates/program-template.php
+++ page-templates/program-template.php
@@ -125,14 +125,14 @@ if ($detect->isMobile())
                     <div class="tab-content" id="mobileSidebarTabContent">
                         <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
                             <?php echo contactInformationBlock(false) ?>
-                            <div class="tab-close-row"><button class="tab-close-button"  style="cursor:pointer" data-role="button" role="button"><i class="fas fa-times"></i> CLOSE</button></div>
+                            <div class="tab-close-row"><button class="tab-close-button"  style="cursor:pointer" onclick="" data-role="button" role="button"><i class="fas fa-times"></i> CLOSE</button></div>
                         </div>
                         <?php if( have_rows('sidebar_links') ): ?>
                         <div class="tab-pane fade" id="menu" role="tabpanel" aria-labelledby="menu-tab">
                             <div class="sidebar-menu">
                             <?php echo getSidebarLinks(false); ?>
                             </div>
-                            <div class="col-sm-12 tab-close-row"><button class="tab-close-button"  style="cursor:pointer" data-role="button" role="button"><i class="fas fa-times"></i> CLOSE</button></div>
+                            <div class="col-sm-12 tab-close-row"><button class="tab-close-button"  style="cursor:pointer" onclick="" data-role="button" role="button"><i class="fas fa-times"></i> CLOSE</button></div>
                         </div>
                         <?php endif; ?>
                     </div>
