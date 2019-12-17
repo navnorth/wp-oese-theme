798b1add936f91b5b24a036c7d93be2b55931331
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2019-12-17 22:10:55 +0800

    remove alert and change inline onclick to close tab function

--- js/front-script.js
+++ js/front-script.js
@@ -143,7 +143,6 @@ jQuery( document ).ready(function() {
     }
     jQuery('.tab-close-button').css('cursor','pointer');
     jQuery(document).on(touchEvent, '.tab-close-button', function(){
-        alert('tab close button');
         close_tab(this);
     });
     
