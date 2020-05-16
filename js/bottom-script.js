60fa5958c2ffef35fee9304119eeac98042eac1c
johnpaulbalagolan <johnpaulbalagolan@gmail.com>
2020-05-17 01:36:56 +0800

    update selector of datatable row onclick event

--- js/bottom-script.js
+++ js/bottom-script.js
@@ -5,7 +5,7 @@ jQuery( document ).ready(function($) {
      $('.slideshow_container .slideshow_slide a').on("focusout",function(){
 	$(this).closest(".slideshow_container").trigger("mouseleave");
      });
-     $(document).on('click', '.wpdt-c .wpDataTables[data-wpdatatable_id=8] table#table_1 tbody tr', function(e){
+     $('.wpdt-c table#table_1_wrapper #table_1[data-wpdatatable_id=8] tbody tr').on('click', function(e){
      	let schoolName = $(this).find('.column-schoolname').text();
      	let state = $(this).find('.column-state').text();
      	let district = $(this).find('.column-district').text();
