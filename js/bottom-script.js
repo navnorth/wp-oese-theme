29ebf72f4646ad1028156f48c38e125d60e6cb03
johnpaulbalagolan <johnpaulbalagolan@gmail.com>
2020-06-10 19:33:49 +0800

    add focus to tool buttons on WPDataTables

--- js/bottom-script.js
+++ js/bottom-script.js
@@ -5,4 +5,10 @@ jQuery( document ).ready(function($) {
      $('.slideshow_container .slideshow_slide a').on("focusout",function(){
 	$(this).closest(".slideshow_container").trigger("mouseleave");
      });
+     /** Add Tab Index to WPDataTables tool buttons **/
+     if ($('.wpDataTables.wpDataTablesWrapper .dt-buttons').length){
+     	$('.wpDataTables.wpDataTablesWrapper .dt-buttons .dt-button').each(function(){
+     		$(this).attr('tabindex','0');
+     	});
+     }
 });
