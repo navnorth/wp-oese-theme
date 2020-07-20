8dded80e9fcd3076ad8512e903cf0d8fbf502f96
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2020-07-21 03:27:07 +0800

    another debug

--- js/bottom-script.js
+++ js/bottom-script.js
@@ -84,15 +84,15 @@ jQuery( document ).ready(function($) {
      $('.wpDataTableFilterBox .wpDataTableFilterSection .wdt-filter-control .dropdown-menu > li > a').focus(function(){
         $(this).closest('div.dropdown-menu').addClass('open');
      });
-     $document.on('keyup','.wpDataTableFilterBox .wpDataTableFilterSection button.dropdown-toggle', function(event){
+     $(document).on('keyup','.wpDataTableFilterBox .wpDataTableFilterSection button.dropdown-toggle', function(event){
           if (event.defaultPrevented) {
                return;
           }
-          /*var key = event.keyCode || event.which || event.key;
+          var key = event.keyCode || event.which || event.key;
 
           if (key===32 || key===13) {
                $(this).closest('.wdt-filter-control').find('.dropdown-menu li.selected a').focus();
-          }*/
+          }
      });
 
      function addRowTabIndex(){
