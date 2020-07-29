649749f8eef8a1c1b9e29484b271e783203daeeb
Joseph Eneldas <joseph.eneldas@navigationnorth.com>
2020-07-29 16:31:23 +0800

    Remove backup files

--- modules/oesepreview (07272020)/admin.js
+++
@@ -1,7 +0,0 @@
-function oesePreviewDraftCopyToClipboard(element) {
-  var copyText = document.getElementById("oese-preview-url-input");
-  copyText.select(); 
-  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-  document.execCommand("copy");
-  alert("Copied Preview URL: " + copyText.value);
-}
