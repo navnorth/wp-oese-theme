faab4376bb1e29224a56df9b163b023f30e97ce1
Joseph Eneldas <joseph.eneldas@navigationnorth.com>
2020-09-16 10:32:18 +0800

    Added script for Browser compatibility Message in WPDataTables

--- js/reroute.js
+++ js/reroute.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function(){
+  /**
+   * WPDT Compare Addon - Retrieve columns for comparison
+   */
   window.retrieveCompareData = function retrieveCompareData(obj,tableDescription){
       console.log('-');
       var tableid = obj.closest('.wpDataTablesWrapper').find('table.wpDataTable').attr('data-wpdatatable_id');
@@ -141,16 +144,66 @@ jQuery(document).ready(function(){
 
   }
   
+  /**
+  * Adds thousand separator to compare addon data with float data type
+  */
   window.thousands_separators = function thousands_separators(num){
     var num_parts = num.toString().split(".");
     num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return num_parts.join(".");
   }
   
+  /**
+  * Adds leading zeros to compare addon data with float data type
+  */
   window.addZeroes  = function addZeroes(num) {
     const dec = num.split('.')[1]
     const len = dec && dec.length > 2 ? dec.length : 2
     return Number(num).toFixed(len)
   }
+  
+  
+  
+
+  /**
+  * detect IE 11 below
+  * returns version of IE or false, if browser is not Internet Explorer
+  */
+  window.wdt_broser_detect = function wdt_broser_detect() {
+    var ua = window.navigator.userAgent;
+    // Test Values
+    // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';  // IE 10
+    // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'; // IE 11
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0'; // Edge 12
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'; // Edge 13
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {  // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) { // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+    
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) { // Edge (IE 12+) => return version number
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    return false; // other browser
+  }
+
+  window.wdt_display_browser_message = function wdt_display_browser_message(){
+    var htm = '<div class="alert alert-warning alert-dismissible fade show"><strong>Warning!</strong> The dynamic table on this page may not display in your browser. We recommend using <a href="https://www.google.com/chrome/" target="_blank">Chrome</a>, <a href="https://www.microsoft.com/en-us/edge" target="_blank">Edge</a>, or <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox<a>.<button type="button" class="close" data-dismiss="alert">&times;</button></div>';
+    if(jQuery('div.wpdt-c').first().length){
+      jQuery(htm).insertBefore(jQuery('div.wpdt-c').first() );
+    }
+  }
+
+  var version = wdt_broser_detect();
+  if (version !== false && version < 12) wdt_display_browser_message();
   
 });
