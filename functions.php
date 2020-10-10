4d8c8f2b58493250a2b4b1522dba8e6708a2a968
Joseph Eneldas <joseph.eneldas@navigationnorth.com>
2020-10-10 12:38:05 +0800

    Restore CSV upload functionality for WordPress 4.9.9 and up

--- functions.php
+++ functions.php
@@ -3052,3 +3052,29 @@ function oese_add_home_detector()  {
   }
 }
 add_action( 'admin_footer', 'oese_add_home_detector' );
+
+/**
+ * Restore CSV upload functionality for WordPress 4.9.9 and up
+ */
+add_filter('wp_check_filetype_and_ext', function($values, $file, $filename, $mimes) {
+	if ( extension_loaded( 'fileinfo' ) ) {
+		// with the php-extension, a CSV file is issues type text/plain so we fix that back to 
+		// text/csv by trusting the file extension.
+		$finfo     = finfo_open( FILEINFO_MIME_TYPE );
+		$real_mime = finfo_file( $finfo, $file );
+		finfo_close( $finfo );
+
+		if ( $real_mime === 'text/plain' && preg_match( '/\.(csv)$/i', $filename ) ) {
+			$values['ext']  = 'csv';
+			$values['type'] = 'text/csv';
+		}
+	} else {
+		// without the php-extension, we probably don't have the issue at all, but just to be sure...
+		if ( preg_match( '/\.(csv)$/i', $filename ) ) {
+			$values['ext']  = 'csv';
+			$values['type'] = 'text/csv';
+		}
+	}
+
+	return $values;
+}, PHP_INT_MAX, 4);
