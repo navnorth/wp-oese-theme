e1a3707e48d12cba599c25b12ee2edb39dd4ee01
josepheneldas <joseph.eneldas@navigationnorth.com>
2021-12-26 23:01:53 +0800

    OESE-205 - Featured item
    - Fixed issue with hiding the inspector controls, wherein the inspector controls from the other blocks gets hidden as well, so I have to find a way using classes to make sure the hiding targets a specific block only.
    - Same goes with the Toolbar.
    - WP-5.7.4 Register_block_type doesn't work inside themes, research says  gutemberg blocks with the block.json descriptor is not intended to be used inside a theme, but it could be a bug at the same time.  Happens no matter what version I use. So for now I resorted to the wp_enqueue_script type of block registration.

--- modules/shortcodesblockv2/featured_item/build/index.asset.php
+++ modules/shortcodesblockv2/featured_item/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-wp.data'), 'version' => '4588fdced77138acf1c7091e97819fff');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '24b0f4c808cc8691491537c03bc35fb9');
