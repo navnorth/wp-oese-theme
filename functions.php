857d02ee2938884339cd3e04751567dd5b1da1e8
josepheneldas <joseph.eneldas@navigationnorth.com>
2022-01-17 16:00:17 +0800

    OESE-205 - Featured item
    - enabled block entry point

--- functions.php
+++ functions.php
@@ -718,7 +718,7 @@ require_once( get_stylesheet_directory() . '/theme-functions/theme-shortcode.php
  **/
  $_vsn = (int)explode('.',get_bloginfo('version'))[0];
  if($_vsn > 4) require_once( get_stylesheet_directory() . '/modules/shortcodesblockv2/accordion/init.php' );
- //if($_vsn > 4) require_once( get_stylesheet_directory() . '/modules/shortcodesblockv2/featured_item/oese-featured-item-block.php' );
+ if($_vsn > 4) require_once( get_stylesheet_directory() . '/modules/shortcodesblockv2/featured_item/oese-featured-item-block.php' );
  if($_vsn > 4) require_once( get_stylesheet_directory() . '/modules/shortcodesblock/shortcodesblock.php' );
  function theme_back_enqueue_script()
 {
