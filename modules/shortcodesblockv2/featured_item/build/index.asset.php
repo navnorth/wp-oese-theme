5e67d438cb0712b5c707f30aad65b46655ee91cf
josepheneldas <joseph.eneldas@navigationnorth.com>
2022-01-22 22:10:16 +0800

    OESE-205 - Featured item
    - Changed the inspector control title
    - Adjusted padding inside the image container which pushes the title to the right when image is empty.

--- modules/shortcodesblockv2/featured_item/build/index.asset.php
+++ modules/shortcodesblockv2/featured_item/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b09a41e8fd5f31b9c4637d0df849a915');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b8153fe23953e1c32ac98a6044f33e2c');
