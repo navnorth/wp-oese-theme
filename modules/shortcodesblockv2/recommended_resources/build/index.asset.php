2b263a488a43516d90a1fd9bb6f57992498979a4
josepheneldas <joseph.eneldas@navigationnorth.com>
2022-01-25 19:47:19 +0800

    OESE-209 - Recommended-Resources
     - Set Word-Break to unset in the block type list
    -  Applied CSS to new paragraphs to align it with the first paragraph style
    - added an image placeholder above the description to add space where users can click on and bring up the block settings
    - Videos and Websites displays differently before, but now both are displaying image, the only difference is we display YT Video Thumbnail for Video and the selected image or placeholder image for Websites.
    - Abandoned the use of YT API and resorted to using the Iframe.
    -  removed the links in the image only in the editor to prevent the page from redirecting when trying to edit.

--- modules/shortcodesblockv2/recommended_resources/build/index.asset.php
+++ modules/shortcodesblockv2/recommended_resources/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '696f68bd0e98104230354a8f008c87d5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '28e292bfd3da001490161c4a5c8bf4e8');
