dab5e50a872d723f85d0e8151e594f881a28fd4a
josepheneldas <joseph.eneldas@navigationnorth.com>
2022-02-07 10:46:53 +0800

    OESE-216 - Publication
    - Changed file selector button label

--- modules/shortcodesblockv2/publication_download/build/index.asset.php
+++ modules/shortcodesblockv2/publication_download/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd2dba3fd34232a3f72766fac8db0c3a7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '70f808d3b75fff0ac5af402b20273df9');
