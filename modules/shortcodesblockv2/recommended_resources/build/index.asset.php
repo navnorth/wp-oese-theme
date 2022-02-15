2fa25452fe76f8f9125a9ab8b5b52c561fa6b521
josepheneldas <joseph.eneldas@navigationnorth.com>
2022-02-15 11:37:37 +0800

    OESE-209 - Recommended-Resources
    - Updated build

--- modules/shortcodesblockv2/recommended_resources/build/index.asset.php
+++ modules/shortcodesblockv2/recommended_resources/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e3464412408737f93b48a19bfdde6c5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0bd78181ff25b519e7c5ddfe9b610a01');
