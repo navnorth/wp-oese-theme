e41912705522e9260b8c89d9597a9eed6fa90992
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-05-25 23:24:43 +0800

    OESE-228 disable accordion preview

--- modules/shortcodesblockv2/accordion_v2/build/index.asset.php
+++ modules/shortcodesblockv2/accordion_v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '498bf6ebbe6a187097cdeb15a02b4711');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '03eb5878be11c12a26c48fc3fd930007');
