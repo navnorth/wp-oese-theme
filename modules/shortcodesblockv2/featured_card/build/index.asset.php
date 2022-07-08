6f437bfab8f7bb1ec967b3ce80b0ae2a2fe66341
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-07-08 23:57:17 +0800

    OESE-235 set image alt text on featured card block when selecting image via media library

--- modules/shortcodesblockv2/featured_card/build/index.asset.php
+++ modules/shortcodesblockv2/featured_card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c5a95a07b0e08206c70b5b1e8db2d405');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2b81efddbdb5af4f1fb72e29d5a5a995');
