e79fc4f01dfe0c17ef9012b9527e2fc0dc966a7f
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-08-04 22:10:12 +0800

    OESE-241 change keypress to keydown to detect arrow key press

--- js/front-script.js
+++ js/front-script.js
@@ -135,7 +135,8 @@ jQuery( document ).ready(function() {
     // Add Keyboard navigation on hamburger menu on mobile
     jQuery('.mobile-nav-bar .navi_icn').attr('tabindex','0');
     jQuery('.mobile-nav-bar .navi_icn').attr('aria-label','menu');
-    jQuery('.mobile-nav-bar .navi_icn').on("keypress", function(e) {
+    jQuery('.mobile-nav-bar .navi_icn').on("keydown", function(e) {
+      console.log(e);
       var code = e.keyCode || e.which;
       if(code == 13 || code == 32) { 
         if (jQuery('.mobile-nav-bar .navi_icn .fa-bars').length>0)
