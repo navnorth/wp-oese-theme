3b32e74deef5b56675daf0b02813115cf9bd90b4
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-11-25 21:42:13 +0800

    OESE-256 add nalrc logo to header

--- template-parts/nalrc/header.php
+++ template-parts/nalrc/header.php
@@ -1,4 +1,8 @@
 <?php 
 /** NALRC Header **/
 ?>
-<div class="nalrc-header">NALRC Header Section</div>
+<div class="nalrc-header">
+	<div class="nalrc-wrapper">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/nalrc-logo.png" alt="Native American Language Resource Center" />
+	</div>
+</div>
