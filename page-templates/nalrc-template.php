abb53350febdf90034b21f762cd7b5195e37d348
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-11-25 21:46:13 +0800

    OESE-257 add wrapper to nalrc page

--- page-templates/nalrc-template.php
+++ page-templates/nalrc-template.php
@@ -6,25 +6,28 @@ global $post;
 
 // Load Default OESE Header
 get_header();
-
-get_template_part('template-parts/nalrc/header');
-
 ?>
-<div id="content" class="row custom-common-padding nalrc-template">
-	<div class="row nalrc-details">
-		<h1 class="page_header"><?php echo $post->post_title; ?></h1>
-		<?php
-		// Load Page Content
-		while (have_posts()):
-			the_post();
-			get_template_part('content','page');
-		endwhile;
-		?>
+<div class="nalrc-template">
+<?php
+	get_template_part('template-parts/nalrc/header');
+?>
+	<div id="content" class="row custom-common-padding">
+		<div class="row nalrc-details">
+			<h1 class="page_header"><?php echo $post->post_title; ?></h1>
+			<?php
+			// Load Page Content
+			while (have_posts()):
+				the_post();
+				get_template_part('content','page');
+			endwhile;
+			?>
+		</div>
 	</div>
-</div>
 <?php 
-get_template_part('template-parts/nalrc/footer');
-
+	get_template_part('template-parts/nalrc/footer');
+?>
+</div>
+<?php
 // Load Default OESE Footer
 get_footer();
 ?>
