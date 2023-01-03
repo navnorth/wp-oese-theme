704992bd1f0d687000991b795e0098beb6513f92
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-01-03 18:07:30 +0800

    OESE-252 fix missing search input field

--- js/nalrc.js
+++ js/nalrc.js
@@ -2,12 +2,13 @@ jQuery(function($){
 	setTimeout(function(){
 		let filter = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter');
 		let label = filter.find('label').text();
+		let input = filter.find('input').html();
 		let tabLabel = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter').closest('.tab-pane').attr('aria-labelledby');
 		let tabLabelText = $('#' + tabLabel).text();
 		tapLabelText = 'Search' + tabLabelText;
-		console.log(tapLabelText);
 		$('.wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox').prepend(filter);
 		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').text(tapLabelText);
+		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').append(input);
 		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > input').attr('placeholder','Search by keyword or phrase');
 	},1000);
 });
