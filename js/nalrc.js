782defa0c4ac2d94b50cf936b2dcb3449164e54e
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-01-03 17:39:37 +0800

    OESE-252 filter section styles

--- js/nalrc.js
+++ js/nalrc.js
@@ -3,8 +3,9 @@ jQuery(function($){
 		let filter = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter');
 		let label = filter.find('label').text();
 		let tabLabel = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter').closest('.tab-pane').attr('id');
-		let tabLabelText = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter > label').text();
+		let tabLabelText = $('#' + tapLabel).text();
 		tapLabelText = 'Search ' + tabLabelText;
+		console.log(tapLabelText);
 		$('.wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox').prepend(filter);
 		$('.page-template-nalrc-template #table_1_wrapper #table_1_filter > label').text(tapLabelText);
 	},1000);
