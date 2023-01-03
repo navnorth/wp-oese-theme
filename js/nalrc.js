361b5c3da2e03981230400ea7390362b1806f3be
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-01-03 17:43:39 +0800

    OESE-252 fix typo

--- js/nalrc.js
+++ js/nalrc.js
@@ -3,7 +3,7 @@ jQuery(function($){
 		let filter = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter');
 		let label = filter.find('label').text();
 		let tabLabel = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter').closest('.tab-pane').attr('id');
-		let tabLabelText = $('#' + tapLabel).text();
+		let tabLabelText = $('#' + tabLabel).text();
 		tapLabelText = 'Search ' + tabLabelText;
 		console.log(tapLabelText);
 		$('.wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox').prepend(filter);
