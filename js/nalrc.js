3440d3fba2b0a4d1056e79697318dd8682c64108
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-01-03 18:15:57 +0800

    OESE-252 debug search input display

--- js/nalrc.js
+++ js/nalrc.js
@@ -5,10 +5,10 @@ jQuery(function($){
 		let input = filter.find('input').html();
 		let tabLabel = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter').closest('.tab-pane').attr('aria-labelledby');
 		let tabLabelText = $('#' + tabLabel).text();
-		tapLabelText = 'Search' + tabLabelText;
+		tabLabelText = 'Search' + tabLabelText + input;
+		console.log(tabLabelText);
 		$('.wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox').prepend(filter);
-		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').text(tapLabelText);
-		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').append(input);
-		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > input').attr('placeholder','Search by keyword or phrase');
+		//$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').text(tabLabelText);
+		//$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > input').attr('placeholder','Search by keyword or phrase');
 	},1000);
 });
