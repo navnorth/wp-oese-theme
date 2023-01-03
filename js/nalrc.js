3c908fb353915144835eeac2d07926832815252c
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-01-03 17:50:00 +0800

    OESE-252 add search input placeholder text

--- js/nalrc.js
+++ js/nalrc.js
@@ -2,11 +2,12 @@ jQuery(function($){
 	setTimeout(function(){
 		let filter = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter');
 		let label = filter.find('label').text();
-		let tabLabel = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter').closest('.tab-pane').attr('id');
+		let tabLabel = $('.page-template-nalrc-template #table_1_wrapper #table_1_filter').closest('.tab-pane').attr('aria-labelledby');
 		let tabLabelText = $('#' + tabLabel).text();
 		tapLabelText = 'Search ' + tabLabelText;
 		console.log(tapLabelText);
 		$('.wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox').prepend(filter);
 		$('.page-template-nalrc-template #table_1_wrapper #table_1_filter > label').text(tapLabelText);
+		$('.page-template-nalrc-template #table_1_wrapper #table_1_filter > input').attr('placeholder','Search by keyword or phrase');
 	},1000);
 });
