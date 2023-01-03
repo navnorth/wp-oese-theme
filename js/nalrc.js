7a8f45c503475fabbecfd817f117e67d27710177
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-01-03 18:24:07 +0800

    OESE-252 another debugging

--- js/nalrc.js
+++ js/nalrc.js
@@ -7,8 +7,9 @@ jQuery(function($){
 		let tabLabelText = $('#' + tabLabel).text();
 		tabLabelText = 'Search' + tabLabelText + input;
 		console.log(tabLabelText);
+		console.log(input);
 		$('.wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox').prepend(filter);
-		//$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').text(tabLabelText);
+		$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > label').text(tabLabelText);
 		//$('.page-template-nalrc-template .wpdt_main_wrapper .wpDataTablesFilter .wpDataTableFilterBox .dataTables_filter > input').attr('placeholder','Search by keyword or phrase');
 	},1000);
 });
