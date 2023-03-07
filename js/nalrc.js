3ef5d31e2dd0a9809d6f46bae3f81279df8e41ce
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-03-07 17:25:40 +0800

    OESE-280 manually hide dropdown on ESC key press

--- js/nalrc.js
+++ js/nalrc.js
@@ -145,7 +145,7 @@ jQuery(function($){
 			} else if (code==27){
 				e.preventDefault();
 				e.stopPropagation();
-				return false;
+				$(this).closest('.bootstrap-select').find('.dropdown-menu').removeClass('open show');
 			} else if (code==9){
 				if (!e.shiftKey){
 					if ($(this).closest('.wpDataTableFilterSection').next().length){
